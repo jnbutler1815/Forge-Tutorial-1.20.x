6336f608d390336f55de22c3e393fa99e9e00289
James Butler <jnbutler1815@gmail.com>
2023-11-30 15:09:43 -0600

    Added second custom block raw_sapphire_block.

--- src/main/java/com/jnbutlerdevelopment/tutorialmod/block/ModBlocks.java
+++ src/main/java/com/jnbutlerdevelopment/tutorialmod/block/ModBlocks.java
@@ -19,9 +19,14 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
 
+    // Add entries for blocks here
     public static final RegistryObject<Block> SAPPHIRE_BLOCK = registerBlock("sapphire_block",
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
 
+    public static final RegistryObject<Block> RAW_SAPPHIRE_BLOCK = registerBlock("raw_sapphire_block",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
+
+    // end of block entries
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn);
--- src/main/java/com/jnbutlerdevelopment/tutorialmod/item/ModCreativeModeTabs.java
+++ src/main/java/com/jnbutlerdevelopment/tutorialmod/item/ModCreativeModeTabs.java
@@ -25,6 +25,8 @@ public class ModCreativeModeTabs {
                                 pOutput.accept(ModItems.SAPPHIRE.get());
                                 pOutput.accept(ModItems.RAW_SAPPHIRE.get());
                                 pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
+                                pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+
                             })
                             .build());
 
