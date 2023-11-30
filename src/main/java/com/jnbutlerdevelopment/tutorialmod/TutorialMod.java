7f8a91314ca49e2a34340549ba16ff15780f977a
James Butler <jnbutler1815@gmail.com>
2023-11-30 14:18:05 -0600

    Added raw sapphire custom block and also added a custom creative mode tab called Sapphire Tutorial Tab

--- src/main/java/com/jnbutlerdevelopment/tutorialmod/TutorialMod.java
+++ src/main/java/com/jnbutlerdevelopment/tutorialmod/TutorialMod.java
@@ -1,6 +1,9 @@
 package com.jnbutlerdevelopment.tutorialmod;
 
+import com.jnbutlerdevelopment.tutorialmod.item.ModCreativeModeTabs;
+import com.jnbutlerdevelopment.tutorialmod.item.ModItems;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -27,6 +30,10 @@ public class TutorialMod
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModCreativeModeTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -42,7 +49,11 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        // code to add an Item to the vanilla creative tabs
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
